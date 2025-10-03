25c5af0ea62acd867c6bbb80bf6f27de0307f3ec
Gonzalo Chica Godino <chicagodinogonzalo@gmail.com>
2025-10-03 09:42:20 +0200

    Actualizada la página sobre mi

--- src/pages/SobreMi.tsx
+++ src/pages/SobreMi.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import miFoto from '../assets/mi-foto.png'; // pon tu foto aquí
+import miFoto from "../assets/mi-foto.png";
 
-const SobreMi: React.FC = () => {
+export default function SobreMi() {
   return (
     <section className="sobre-mi">
       <img src={miFoto} alt="Foto de Gonzalo" className="foto" />
-      <h1>¡Hola! Soy Gonzalo</h1>
-      <p>Estudiante de 2º DAM. Apasionado por la programación y en constante aprendizaje.</p>
-      <p>Tecnologías: Java, SQL, HTML, CSS, JavaScript, React, Spring.</p>
+      <h2>Hola, soy Gonzalo</h2>
+      <p>
+        Tengo 18 años, estoy terminando el FP Superior en Desarrollo de Aplicaciones Multiplataforma (DAM).
+        Me apasiona la programación y actualmente estoy aprendiendo tecnologías como:
+      </p>
+      <ul>
+        <li>Java</li>
+        <li>JavaScript y TypeScript</li>
+        <li>React</li>
+        <li>Spring Boot</li>
+        <li>HTML y CSS</li>
+        <li>SQL y MySQL</li>
+      </ul>
+      <p>
+        Mi objetivo es crecer como desarrollador, construir proyectos útiles y conseguir mi primer trabajo
+        profesional al terminar el FP. Además, me gusta aprender de forma autodidacta, hacer portfolio en GitHub
+        y experimentar con nuevas tecnologías.
+      </p>
+      <p>
+        Soy proactivo, con buena capacidad de resolución de problemas y siempre buscando mejorar mis habilidades.
+      </p>
     </section>
   );
 }
-
-export default SobreMi;
