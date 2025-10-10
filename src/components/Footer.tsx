1a6d1aea0a48a899a0b4e78c3b164350cc2c9115
Gonzalo Chica Godino <chicagodinogonzalo@gmail.com>
2025-10-10 14:05:57 +0200

    Nuevo active para el header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 const Footer: React.FC = () => {
     return (
         <footer className="footer">
-        <p>© 2024 Gonzalo Chica Godino. Todos los derechos reservados.</p>
+        <p>© 2025 Gonzalo Chica Godino. Todos los derechos reservados.</p>
         <div className="socials">
             <a href="https://github.com/gonzalocg123" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
             <FaGithub />
